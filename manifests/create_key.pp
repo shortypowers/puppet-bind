1d2b86f2e2352fe40f3254ee418a722dbd6917a9
Tim Rudolph <trudolph@forsythe.com>
2015-04-06 18:35:41 -0700

    added create_resources section, renamed key.pp

--- manifests/create_key.pp
+++ manifests/create_key.pp
@@ -48,3 +48,11 @@ define bind::key (
         }
     }
 }
+
+class bind::create_key {
+  # pull and merge values from bind::key hash
+  $hiera_config = hiera_hash('bind::key')
+  if $hiera_config {
+    create_resources(bind::key, $hiera_config)
+  }
+}
